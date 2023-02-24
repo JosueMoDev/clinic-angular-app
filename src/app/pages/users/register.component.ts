de986384f81befb0dc961a56f77ab4e242a8e994
JosueMoDev <82752441+JosueMoDev@users.noreply.github.com>
2023-02-24 12:03:10 -0600

    User Form Validations

--- src/app/pages/users/register.component.ts
+++ src/app/pages/users/register.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Component, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
 import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 import { UserService } from 'src/app/services/user.service';
 
@@ -15,7 +15,7 @@ import { UserService } from 'src/app/services/user.service';
     },
   ]
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
   public isFirstStepValid: string = '';
   public isSecondStepValid: string = '';
   public document_type:string = 'DUI';
@@ -23,22 +23,28 @@ export class RegisterComponent {
   public currentStep : number = 1  ;
   public formSubmitted:boolean = false;
   
-  public registerForm: FormGroup = this.formbuilder.group({
-    personalInformation: this.formbuilder.group({
-      document_type: ['DUI', Validators.required],
-      user_id: ['04850790-7', Validators.required],
-      mail_provider:['@gmail.com', Validators.required],
-      email: ['jonasmorales', Validators.required],
-      name: ['jonas', Validators.required],
-      lastname: ['morales', Validators.required],
-      phone: ['', Validators.required],
-      gender: ['', Validators.required],
-    }),
-    rol: ['',Validators.required],
-    file:['']
-
-  });
+  public registerForm!: FormGroup;
+    
+ 
+     
   ngOnInit() {
+    this.registerForm= this.formbuilder.group({
+      personalInformation: this.formbuilder.group({
+        document_type: ['DUI', Validators.required],
+        user_id: ['04850790-7', Validators.required],
+        mail_provider: ['@gmail.com', Validators.required],
+        email: ['jonasmorales', Validators.required],
+        name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(30), this.forbiddenNameValidator( /^[(! " # $ % & ' * + , -. / : ; < = > ?^@ _ `{}|)+(0-9)+(0-9,0-9)+((! " # $ % & ' * + , -. / : ; < = > ?^@ _ `{}|),(! " # $ % & ' * + , -. / : ; < = > ?^@ _ `{}|))]/)]],
+        lastname: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(30)] ],
+        phone: ['', Validators.required],
+        gender: ['', Validators.required],
+       // \  [ ] ^  { | }}
+      }),
+      rol: ['',Validators.required],
+      file:['']
+  
+    });
+
     this.registerForm.get('personalInformation')?.statusChanges.subscribe(status => this.isFirstStepValid = status)
     this.registerForm.get('rol')?.statusChanges.subscribe(status => this.isSecondStepValid = status)
     this.registerForm.get('personalInformation.document_type')?.valueChanges.subscribe(value => this.document_type = value) 
@@ -57,6 +63,18 @@ export class RegisterComponent {
     this.userservice.crearteNewUserWithEmailAndPassword(this.registerForm.value)
      
   }
+
+  get name() { return this.registerForm.get('personalInformation.name'); }
+  get lastname(){ return this.registerForm.get('personalInformation.lastname'); }
+  
+  forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const forbidden = nameRe.test(control.value);
+      console.log(forbidden ? {forbiddenName: {value: control.value}} : null)
+      return forbidden ? {forbiddenName: {value: control.value}} : null;
+    };
+  }
+  
 
   nextPage() {
     if (  this.isFirstStepValid ==='VALID' || this.isSecondStepValid==='VALID' ) {
