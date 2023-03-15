8cfe46986a5159f103f857a474c0567f38384871
JosueMoDev <82752441+JosueMoDev@users.noreply.github.com>
2023-03-15 16:17:26 -0600

    @style sidebar impruvement

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UiService } from '../services/ui.service';
 
 @Component({
   selector: 'app-pages',
@@ -7,5 +8,7 @@ import { Component } from '@angular/core';
   ]
 })
 export class PagesComponent {
-
+  constructor(
+    public ui: UiService
+  ){}
 }
