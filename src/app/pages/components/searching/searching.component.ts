7ee5b117b6a025a1e89cccdc9c53a48a34459322
JosueMoDev <82752441+JosueMoDev@users.noreply.github.com>
2023-04-01 16:59:34 -0600

    @feature browser has implemented

--- src/app/pages/components/searching/searching.component.ts
+++ src/app/pages/components/searching/searching.component.ts
@@ -1,8 +1,11 @@
-import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
-import {FormControl} from '@angular/forms';
-import {debounceTime, finalize, map, switchMap, tap} from 'rxjs/operators';
-import {fromEvent, of} from 'rxjs';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { debounceTime, switchMap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 import { SearchingService } from 'src/app/services/searching.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatMenuTrigger} from '@angular/material/menu';
+import { User } from '../../../models/user.model';
+import { UpdateProfileService } from 'src/app/services/update-profile.service';
 
 @Component({
   selector: 'app-searching',
@@ -11,17 +14,41 @@ import { SearchingService } from 'src/app/services/searching.service';
   ]
 })
 export class SearchingComponent {
-  public isLoading = false;
-  public src!: string;
-  public data$: any;
-  constructor(private searchingService: SearchingService) {}
-  search(value: any) {
-    console.log(value)
-    this.isLoading = true;
+  @ViewChild(MatMenuTrigger) menuTrigger!: MatMenuTrigger;
+  public searchForm!: FormGroup;
+  public searchResults$!: Observable<User[]> | undefined;
+  public data = []
 
-    // this.data$ = this.apiRest.searchTrack({query: value}).pipe(
-    //   map(({tracks}) => tracks.items),
-    //   finalize(() => this.isLoading = false)
-    // )
+
+  constructor(
+    private searchingService: SearchingService,
+    private formBuilder: FormBuilder,
+    public updateProfileService: UpdateProfileService
+  ) { }
+
+  ngOnInit(){
+    this.searchForm = this.formBuilder.group({
+      searchInput: ['']
+    });
+    this.searchResults$ = this.searchForm.get('searchInput')?.valueChanges.pipe(
+      debounceTime(500),
+      switchMap(async (searchText: string) => {
+        return await this.searchAsync(searchText);
+      })
+    );
   }
-}
+
+  async searchAsync(searchText: string): Promise<User[]> {
+    if (!searchText) {
+      this.menuTrigger.closeMenu();
+    }
+    this.menuTrigger.openMenu();
+    this.searchingService.getResponse(searchText).subscribe(
+      (resp: any) => { this.data = resp.data })
+      if (!this.data.length) {
+        this.menuTrigger.closeMenu();
+      }
+      return [...this.data] ;
+  }
+    
+} 
--- src/app/services/searching.service.ts
+++ src/app/services/searching.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+import { environment } from 'src/environments/environment';
+import { delay, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchingService {
+  public headers: {} = this.authService.headers
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService
+  ) { }
 
-  constructor() { }
+  getResponse(term: string) {
+    return this.http.get(`${environment.THECLINIC_API_URL}/search/${term}`, this.headers)
+  }
 }
