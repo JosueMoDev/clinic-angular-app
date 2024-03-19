e63abdac57818b23dd4bf532a23a20c3007cf02f
Jonas Morales <josuemodev@gmail.com>
2024-03-19 11:02:43 -0600

    authentication standalone component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -11,18 +11,16 @@ declare const google: any;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styles: [
-  ]
+  styleUrls: ['./login.component.css'],
 })
-
 export class LoginComponent implements OnInit, AfterViewInit {
   public loginForm!: FormGroup;
   public currentRoute!: string;
-  public routeSubs$!: Subscription
-  public type : string ='password'
+  public routeSubs$!: Subscription;
   public visibility: boolean = true;
-  public error : any = error
-  
+  public error: any = error;
+  public type: string = 'password';
+  hide = true;
 
   @ViewChild('googleLogin') googleLogin!: ElementRef;
 
@@ -31,23 +29,23 @@ export class LoginComponent implements OnInit, AfterViewInit {
     private authService: AuthService,
     private router: Router,
     private _ngZone: NgZone
-  ) { 
-    this.routeSubs$ = this.router.events.subscribe(
-      (event: Event) => {
-        if (event instanceof NavigationEnd) {
-          this.currentRoute = event.url
-        }
+  ) {
+    this.routeSubs$ = this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.url;
       }
-    );
+    });
     this.routeSubs$.unsubscribe;
-  } 
+  }
 
   ngOnInit(): void {
-
     this.loginForm = this.formBuider.group({
-      email: [localStorage.getItem('user_email'), [Validators.required, Validators.email]],
+      email: [
+        localStorage.getItem('user_email'),
+        [Validators.required, Validators.email],
+      ],
       password: [null, [Validators.required, Validators.minLength(8)]],
-      rememberme: [localStorage.getItem('rememberme-state')]
+      rememberme: [localStorage.getItem('rememberme-state')],
     });
   }
 
@@ -55,61 +53,64 @@ export class LoginComponent implements OnInit, AfterViewInit {
     this.googleInit();
   }
 
-  googleInit() { 
+  googleInit() {
     google.accounts.id.initialize({
-        client_id: environment.GOOGLE_CLIENT_KEY,
-        callback: (resp:any) => this.handleCredentialResponse(resp)
+      client_id: environment.GOOGLE_CLIENT_KEY,
+      callback: (resp: any) => this.handleCredentialResponse(resp),
     });
-    google.accounts.id.renderButton(
-        this.googleLogin.nativeElement,
-        { theme: "dark", size: "large" }  
-    );
+    google.accounts.id.renderButton(this.googleLogin.nativeElement, {
+      theme: 'dark',
+      size: 'large',
+    });
   }
 
   async handleCredentialResponse(response: any) {
-    await this.authService.googleSingIn(response.credential, this.currentRoute)
-    .subscribe((response: any) => {
-      if (response.ok) {
-        this._ngZone.run(
-          () => {
-            this.router.navigate(['/']);
-          })
-        success(response.message);
-      }
-    },
-      (error: any) => this.error(error.error.message)
-    );
+    await this.authService
+      .googleSingIn(response.credential, this.currentRoute)
+      .subscribe(
+        (response: any) => {
+          if (response.ok) {
+            this._ngZone.run(() => {
+              this.router.navigate(['/']);
+            });
+            success(response.message);
+          }
+        },
+        (error: any) => this.error(error.error.message)
+      );
   }
 
-
-  get email() { return this.loginForm.get('email') };
-  get password() { return this.loginForm.get('password') };
-  get rememberme() { return this.loginForm.get('rememberme')?.value };
-
-  changeVisibility() {
-    this.visibility= !this.visibility;
-    (this.type === 'password') ? this.type = 'text' : this.type = 'password';
+  get email() {
+    return this.loginForm.get('email');
   }
- 
-  
-  loginWithEmailAndPassword() { 
-    this.authService.loginWithEmailAndPassword(this.loginForm.value, this.currentRoute)
-    .subscribe( (resp:any) => {
-      if (resp.account) {
-        if (this.rememberme) {
-          localStorage.setItem('rememberme-state', this.rememberme);
-          localStorage.setItem('user_email', this.email?.value);
-        } else if (!this.rememberme) {
-          localStorage.removeItem('rememberme-state');
-          localStorage.removeItem('user_email');
+  get password() {
+    return this.loginForm.get('password');
+  }
+  get rememberme() {
+    return this.loginForm.get('rememberme')?.value;
+  }
+
+  loginWithEmailAndPassword() {
+    this.authService
+      .loginWithEmailAndPassword(this.loginForm.value, this.currentRoute)
+      .subscribe(
+        (resp: any) => {
+          if (resp.account) {
+            if (this.rememberme) {
+              localStorage.setItem('rememberme-state', this.rememberme);
+              localStorage.setItem('user_email', this.email?.value);
+            } else if (!this.rememberme) {
+              localStorage.removeItem('rememberme-state');
+              localStorage.removeItem('user_email');
+            }
+            this.loginForm.reset();
+            this.router.navigateByUrl('/');
+            success(resp.message);
+          }
+        },
+        (error) => {
+          this.error(error.error.message);
         }
-        this.loginForm.reset();
-        this.router.navigateByUrl('/'); 
-        success(resp.message);
-      }
-    },
-    (error) => { this.error(error.error.message)}
-    );
+      );
   }
-
 }
